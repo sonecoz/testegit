03a3da972b21945c530a62e7eeebcce96e9e044e
Brunno Alves <brunnoa97@gmail.com>
2024-07-11 21:56:13 -0300

    testegit

---
+++ Programas/mergulhe em programação com JavaScript/logica-js-aula_1/logica-js-aula_1/app.js
@@ -0,0 +1,73 @@
+// let nome = 'Brunno';
+// let lingProg;
+
+
+// alert (`Olá, ${nome}`);
+
+// lingProg = prompt('Qual a linguagem de programação que você mais gosta?');
+
+
+
+// console.log(`OLá, ${nome} seu codigo de programação fav é ${lingProg}`);
+
+
+// let valor1 = parseInt(Math.random() * 100 + 1);
+// let valor2 = parseInt(Math.random() * 100 + 1);
+// let resultado = valor1 + valor2;
+
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`)
+
+// console.log(valor1)
+// console.log(valor2)
+
+
+// let idade;
+
+// idade = prompt('Quantos anos voce tem?')
+
+
+//     if (idade > 18) {
+//         alert('Você é maior de idade');
+//     } else {
+//         alert('voce é menor de idade');
+//     }
+    
+
+// console.log(idade)
+
+
+// let numero;
+
+// numero = prompt('Digite um numero')
+
+
+//     if (numero > 1) {
+//         alert('Numero positivo');
+//     } else if (numero < 0) {
+//         alert('Numero negativo');
+//     } else {
+//         alert('esse numero é 0')
+//     }
+    
+
+// let numero = 1;
+
+// while (numero < 100) {
+// console.log(numero)
+//     numero++
+// }
+
+
+// let nota;
+
+// nota = prompt('Digite um valor')
+
+// if (nota >= 7) {
+//     alert('aprovado'); 
+// } else {
+//     alert('reprovado')
+// }        
+
+let numero = parseInt(Math.random() * 1000 + 1);
+
+console.log(numero)
---
+++ Programas/mergulhe em programação com JavaScript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,21 @@
+/** alert("Boas vindas ao jogo do número secreto!");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 e 10");
+
+if (chute == numeroSecreto) {
+    console.log("Você descobriu o numero secreto (5)");
+}
+**/
+
+alert("Bem vindo");
+let nome = Lua;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campo")
+
+let mensagemDeErro = "Erro! Preencha todos os campos";
+let idade = prompt('Digite a sua idade');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}
